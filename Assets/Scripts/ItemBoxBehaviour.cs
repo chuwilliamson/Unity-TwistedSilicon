9e62183529ee76acfe5457cefa67e999ff18bf10
BeauxRome <beaux1rome@gmail.com>
2019-07-03 17:31:19 -0500

    Transferred art
    The artists' work has been nrought to mine, so I'm working on setting items to spawn correctly.
    More work and contemplation is definitely required

---
+++ Assets/Scripts/ItemBoxBehaviour.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBoxBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    GameObject crystal;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //carValues will be a classsss/behaviour (not sure which) that will hold the item of the user
+            //itemValue will be that int val
+            //if (other.gameObject.carValues.itemValue != 0)
+            //{
+            //    other.gameObject.carValues.itemValue = Random.Range(1, 100);
+            //    if (other.gameObject.carValues.itemValue <=1 && 
+            //        other.gameObject.carValues.itemValue >= 20)
+            //    {
+            //        crystal.ActivateTrigger.Target = 
+            //    }
+            //    if (other.gameObject.carValues.itemValue <= 21 && 
+            //        other.gameObject.carValues.itemValue >= 40)
+            //    {
+
+            //    }
+            //    if (other.gameObject.carValues.itemValue <= 41 && 
+            //        other.gameObject.carValues.itemValue >= 60)
+            //    {
+
+            //    }
+            //    if (other.gameObject.carValues.itemValue <= 61 && 
+            //        other.gameObject.carValues.itemValue >= 80)
+            //    {
+
+            //    }
+            //    if (other.gameObject.carValues.itemValue <= 81 && 
+            //        other.gameObject.carValues.itemValue >= 100)
+            //    {
+
+            //    }
+            //}
+
+            // Icon = GetComponent(thething)
+            // Icon.Enable;
+
+            // Ideas: Create random definition for Target Item
+            //        Create random Power_prefab for each item, then spawn when necessary
+
+        }
+    }
+
+}
