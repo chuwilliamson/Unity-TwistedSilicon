b79f2cf6a466477ebe35690629ebe79cf87b54a9
JMicahMan <j.micah.manzanares@gmail.com>
2019-07-03 17:31:19 -0500

    Wall AI is working fairly smoothly

---
+++ Assets/Scripts/DeathWallBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DeathWallBehaviour : MonoBehaviour
+{
+
+    public GameObject[] Waypoint;
+    private int curntpoint = 0;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.autoBraking = false;
+        //doing this keeps it seek instead of arrive^
+
+
+        NextPoint();
+    }
+
+    void NextPoint()
+    {
+
+        if (Waypoint.Length == 0)
+        {
+            return;
+        }
+        //incase No points are set up^
+
+        agent.destination = Waypoint[curntpoint].transform.position;
+
+        curntpoint = (curntpoint + 1) % Waypoint.Length;
+    }
+
+    void Update()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 50)
+        {
+            NextPoint();
+        }
+    }
+}
