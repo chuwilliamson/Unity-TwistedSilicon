4e7dcf83d9272551b889c1a4c0d4a8f5480333e5
JMicahMan <j.micah.manzanares@gmail.com>
2019-07-08 17:49:04 -0500

    all is rescaled. need to finish up lap counter

---
+++ Assets/Scripts/CounterBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class CounterBehaviour : MonoBehaviour {
+    public GameObject[] finishLine;
+    public int lapCounter = 0;
+//    private NavMeshAgent agent;
+
+
+    void Start ()
+    {
+       // agent = GetComponent<NavMeshAgent>();
+        goUp();
+    }
+
+    void goUp()
+    {
+        if (finishLine.Length == 5)
+        {
+            lapCounter++;
+        }
+
+        //agent.destination = finishLine[0].transform.position;
+
+    }
+
+    void Update ()
+    {
+     //   if (!agent.pathPending && agent.remainingDistance < 50)
+      //  {
+            goUp();
+     //   }
+
+    }
+}
