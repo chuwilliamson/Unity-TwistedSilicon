117849cb9aff7088e8092a9f1a429acca957234c
JMicahMan <j.micah.manzanares@gmail.com>
2019-07-19 17:22:12 -0500

    Controller scene and main menu buttons added

--- Assets/Scripts/DeathWallBehaviour.cs
+++ Assets/Scripts/DeathWallBehaviour.cs
@@ -169,4 +169,9 @@ public class DeathWallBehaviour : MonoBehaviour
         }
 
     }
+
+
+
+
+
 }
---
+++ Assets/Scripts/GoBackBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoBackBehaviour : MonoBehaviour
+{
+    public void Go()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+    public void GoC()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
+
