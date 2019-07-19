fa7cb5398d27a32fee8eb51345bd9136ee977c2a
BeauxRome <beaux1rome@gmail.com>
2019-07-19 18:24:40 -0500

    aiiiiiiiiie
    Minimap addition

---
+++ Assets/Scripts/MiniMapPointBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapPointBehaviour : MonoBehaviour
+{
+    
+
+    [SerializeField]
+    GameObject car;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(car.transform.position.x, transform.position.y, car.transform.position.z);
+        transform.rotation = new Quaternion(transform.rotation.x, car.transform.rotation.y, transform.rotation.z, transform.rotation.w);
+
+    }
+}
